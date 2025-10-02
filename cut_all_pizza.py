bf2f72a095444a3b0a60838c76fed5f1a5a47adf
Xander <xanderperry2003@gmail.com>
2025-10-02 12:33:05 +0200

    Refactor cut_all_pizza.py for improved dataset processing; remove unused test files and streamline image handling

--- cut_all_pizza.py
+++ cut_all_pizza.py
@@ -1,33 +1,26 @@
 import cv2
 import glob
-import shutil
+import os
 
+import pizza_cutter
+
+DIRECTORY = "C:/Users/Xande/EE cursusmateriaal/3A/Beeldherkenning/PizzaVisionRepo/Pizza_vision/"
 KINDS = ["che", "fun", "haw", "mar", "moz", "sal"]
 
 for kind in KINDS:
-    for_test = True
-    i = 0
-    DIRECTORY = "C:/Users/Xande/EE cursusmateriaal/3A/Beeldherkenning/PizzaVisionRepo/Pizza_vision/"
+    print("starting " + kind)
+    for dataset in ["test", "train", "validation"]:
+        i = 0
+        for filename_old in glob.glob("data/" + kind + "/" + dataset + "/**/*.jpg", recursive=True):
+            filename_new ="data_cutout" + filename_old[filename_old.find("data")+4:]
+            filename_new = filename_new.replace("\\", "/")
+            
+            img = cv2.imread(filename_old)
 
-    for filename_old in glob.glob("data/" + kind + "/train/**/*.jpg", recursive=True):
-        filename_new = DIRECTORY + "data_split/" + kind + "/train/" + filename_old[filename_old.find("\\")+1:filename_old.find("\\")+14] + str(i).zfill(4) + ".jpg"
-        i+=1
-        
-        # print(filename_new)
-        shutil.copy2((DIRECTORY + filename_old), filename_new)
+            img_cutout = pizza_cutter.cut_pizza(img)
 
+            loc = os.path.join(DIRECTORY, filename_new)
 
-    j = 0
-    for filename_old in glob.glob("data/" + kind + "/test/**/*.jpg", recursive=True):
-        if for_test:
-            filename_new = DIRECTORY + "data_split/" + kind + "/test/" + filename_old[filename_old.find("\\")+1:filename_old.find("\\")+14] + str(j).zfill(4) + ".jpg"
-            for_test = False
-        else:
-            filename_new = DIRECTORY + "data_split/" + kind + "/validation/" + filename_old[filename_old.find("\\")+1:filename_old.find("\\")+14] + str(j).zfill(4) + ".jpg"
-            for_test = True
-            j+=1
+            ret = cv2.imwrite(loc, img_cutout)
 
-
-        # print(filename_new)
-        shutil.copy2((DIRECTORY + filename_old), filename_new)
-
+            print(ret, os.path.join(DIRECTORY, filename_new))
